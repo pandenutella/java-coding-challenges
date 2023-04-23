f70ec97ac8e91c7844b8c9c9e752f4b4f4a3f537
johnemmanuelb <johnemmanuel.bacalla@gmail.com>
2023-04-24 00:26:10 +0800

    Add empty sort function and unit tests for Lexicographical (but modified) sorting challenge

---
+++ src/main/java/com/pdn/challenge/LexicographicallyModifiedSorter.java
@@ -0,0 +1,9 @@
+package com.pdn.challenge;
+
+import java.util.List;
+
+public class LexicographicallyModifiedSorter {
+    public List<String> sort(List<String> inputList) {
+        return null;
+    }
+}
